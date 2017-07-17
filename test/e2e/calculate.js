a4fdf1b114ebee04be66432337b13f9eb9e2df54
Minh Phuong <mphuong@kent.edu>
2017-07-17 13:43:34 -0700

    All test passed

--- test/e2e/calculate.js
+++ test/e2e/calculate.js
@@ -7,6 +7,24 @@ module.exports = {
       .waitForElementVisible('body')
       .assert.title('VOC Calculation')
       .assert.elementPresent('input[id="percentCar"]')
+      .setValue('#percentCar', '89')
+      .setValue('#percentSUTruck', '6')
+      .setValue('#percentCTruck', '5')
+      .setValue('#initial', '55')
+      .setValue('#reduce', '40')
+      .click('#submitButton')
+      .assert.containsText('#resultTotal',
+        '193.91')
+      .assert.containsText('#resultCar',
+        '125.86')
+      .assert.containsText('#resultSUTruck',
+        '12.94')
+      .assert.containsText('#resultCTruck',
+        '55.05')
+      .assert.containsText('#resultDay',
+        '193.85')
+      .assert.containsText('#resultMonth',
+        '11631.00')
       .saveScreenshot(config.imgpath(browser) + 'title-presented.png')
       .end();
   }
